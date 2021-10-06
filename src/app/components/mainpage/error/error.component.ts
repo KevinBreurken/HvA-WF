c5e8818a0218cf510a39b345ef54e26a6e5923ac
Kevin Breurken <kevinbreurken@gmail.com>
2021-10-06 22:52:18 +0200

    Added pathname to error display text.

--- src/app/components/mainpage/error/error.component.ts
+++ src/app/components/mainpage/error/error.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent {
 
   private pathArray = window.location.href.split('#');
-  private wrongPathName = ""
+  private wrongPathName = "";
 
-  constructor() {
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      this.pathArray = window.location.href.split('#');
+      this.wrongPathName = this.pathArray[this.pathArray.length-1];
+    });
   }
 
-  ngOnInit(): void {
-    this.wrongPathName = this.pathArray[this.pathArray.length-1];
-  }
 
   public getWrongPathName() {
-    return this.wrongPathName
+    return this.wrongPathName;
   }
 
 }
