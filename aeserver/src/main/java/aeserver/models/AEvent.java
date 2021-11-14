9aefb4eb41bf58acef04ed513886389348f87277
Kevin Breurken <kevinbreurken@gmail.com>
2021-11-14 22:34:12 +0100

    Worked together with Irene on CodeWithMe

--- aeserver/src/main/java/aeserver/models/AEvent.java
+++ aeserver/src/main/java/aeserver/models/AEvent.java
@@ -1,5 +1,7 @@
 package aeserver.models;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import java.util.Date;
 import java.util.Objects;
 import java.util.Random;
--- aeserver/src/main/java/aeserver/restcontroller/AEventsController.java
+++ aeserver/src/main/java/aeserver/restcontroller/AEventsController.java
@@ -5,6 +5,7 @@ import aeserver.exceptions.ResourceNotFoundException;
 import aeserver.models.AEvent;
 import aeserver.repositories.AEventsRepository;
 import aeserver.repositories.AEventsRepositoryMock;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,21 +22,21 @@ public class AEventsController {
     repository = new AEventsRepositoryMock();
   }
 
-  @GetMapping("rest/aevent")
+  @GetMapping("aevent")
   public List<AEvent> getAllAEvents() {
     if (repository.findAll().size() == 0) throw new ResourceNotFoundException("No events to be found.");
 
     return repository.findAll();
   }
 
-  @GetMapping("rest/aevent/{id}")
+  @GetMapping("aevent/{id}")
   public AEvent getEvent(@PathVariable int id){
     if (repository.findById(id) == null) throw new ResourceNotFoundException("id-"+id);
 
     return repository.findById(id);
   }
 
-  @PostMapping("/rest/aevent")
+  @PostMapping("aevent")
   public ResponseEntity<AEvent> setEvent(@RequestBody AEvent event) {
     if(event.getID() == 0)
       event.setID(AEvent.getNextAvailableId());
