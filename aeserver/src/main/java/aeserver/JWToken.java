78e65abb1300c500ecdfeb1a8a55806f9ada15ae
Kevin Breurken <kevinbreurken@gmail.com>
2021-12-20 21:52:56 +0100

    Finished 4.3.1

--- aeserver/src/main/java/aeserver/JWToken.java
+++ aeserver/src/main/java/aeserver/JWToken.java
@@ -1,7 +1,52 @@
 package aeserver;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JWToken {
+
+  private static final String JWT_USERNAME_CLAIM = "sub";
+  private static final String JWT_USERID_CLAIM = "id";
+  private static final String JWT_ADMIN_CLAIM = "admin";
+
+  @Value("${jwt.issuer.MyOrganisation}")
+  private String issuer;
+
+  @Value("${jwt.pass-phrase}")
+  private String passphrase;
+
+  @Value("${jwt.duration-of-validity:1200}")
+  private int tokenDurationOfValidity;
+
+  private static Key getKey(String passPhrase) {
+    byte hmacKey[] = passPhrase.getBytes(StandardCharsets.UTF_8);
+    Key key = new SecretKeySpec(hmacKey, SignatureAlgorithm.HS512.getJcaName());
+    return key;
+  }
+
+  public String encode(String userName, String id, boolean admin) {
+    Key key = getKey(passphrase);
+
+    String token = Jwts.builder()
+      .claim(JWT_USERNAME_CLAIM, userName)
+      .claim(JWT_USERID_CLAIM, id)
+      .claim(JWT_ADMIN_CLAIM, admin)
+      .setIssuer(issuer)
+      .setIssuedAt(new Date())
+      .setExpiration(new Date(System.currentTimeMillis() + tokenDurationOfValidity * 1000L))
+      .signWith(key, SignatureAlgorithm.HS512)
+      .compact();
+
+    return token;
+  }
 }
--- aeserver/src/main/java/aeserver/restcontroller/AuthenticateController.java
+++ aeserver/src/main/java/aeserver/restcontroller/AuthenticateController.java
@@ -1,26 +1,32 @@
 package aeserver.restcontroller;
 
+import aeserver.JWToken;
 import aeserver.exceptions.UnAuthorizedException;
 import aeserver.models.User;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticateController {
+
+  @Autowired
+  JWToken tokengenerator;
 
   @PostMapping("authenticate/login")
   public ResponseEntity<User> login(@RequestBody ObjectNode json) {
     String email = json.get("eMail").asText();
     String passWord = json.get("passWord").asText();
 
-    if (passWord.equals(email.split("@")[0]))
-      return ResponseEntity.accepted().body(new User(email, passWord));
-    else throw new UnAuthorizedException("Cannot authenticate user by email=" + email + " and password=" + passWord);
+    if (passWord.equals(email.split("@")[0])) {
+      User user = new User(email, passWord);
+      String tokenString = tokengenerator.encode(user.getName(),user.getId().toString(),user.isAdmin());
+      return ResponseEntity.accepted().header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenString).body(user);
+    } else throw new UnAuthorizedException("Cannot authenticate user by email=" + email + " and password=" + passWord);
 
   }
 }
